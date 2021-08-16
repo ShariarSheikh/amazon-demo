942d6adf00aec81ba330fcaa5f1e1150dbc4819c
nahid-sheikh <nahidsheikh.shariar.com>
2021-08-16 18:45:07 +0600

    added env.local and customize readme.md file

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,22 +1,13 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-// const config = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STOREGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-//   appId: process.env.REACT_APP_APP_ID,
-// };
-
 const config = {
-  apiKey: "AIzaSyCxMILGEg0qgAhwcDbtdTgYIiphez5_zTY",
-  authDomain: "ecommerce-138ad.firebaseapp.com",
-  projectId: "ecommerce-138ad",
-  storageBucket: "ecommerce-138ad.appspot.com",
-  messagingSenderId: "609957822382",
-  appId: "1:609957822382:web:b904208ce7160918d5123f",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STOREGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(config);
